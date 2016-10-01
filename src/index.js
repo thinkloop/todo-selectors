b902d40d2095e3f1fb5427a83e8a3e21417d5376
Baz <baz@thinkloop.com>
2016-10-01 06:04:39 +0000

    added state exports

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import combineSelectors from 'combine-selectors';
-import { state } from 'todo-redux-state';
+import { state, actions, subscribe, constants } from 'todo-redux-state';
 
 import selectedPage from './site/selected-page';
 import url from './site/url';
 import siteHeader from './site/site-header';
 import todos from './todos/todos';
 
-const selectors = {
+const rawSelectors = {
 	selectedPage,
 	url,
 	siteHeader,
 	todos
 };
 
-export default combineSelectors(selectors, () => state);
+const selectors = combineSelectors(rawSelectors, () => state);
+
+const final = {
+	selectors,
+	actions,
+	constants,
+	subscribe
+};
+
+Object.defineProperty(final, "state", { get: () => state });
+
+export default final;
+
+export {
+	selectors,
+	actions,
+	constants,
+	subscribe
+}
+
+Object.defineProperty(exports, "state", { get: () => state });
